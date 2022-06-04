a02b741cbcfb4e3a20d3043bc541332abbf86017
Evgeniy <aesto161@gmail.com>
2022-06-05 00:49:16 +0300

    policy

--- application/api/client/user.py
+++ application/api/client/user.py
@@ -1,14 +1,42 @@
 # from tkinter import N
 import json
 from flask_login import current_user
+from flask_mail import Mail
 from sqlalchemy import null
 from ...models import Customer
-from ..utils import return_kr, db_commit, email_sendRecovery
+from ..utils import return_kr, db_commit
 from ..utils import Kitchen_response as kr
-from flask import jsonify, redirect, session, request
+from flask import current_app, jsonify, redirect, session, request
 from hashlib import pbkdf2_hmac
 from datetime import datetime
-# RecoveryLink
+from flask_mail import Message
+# from ... import mail
+
+
+# def email_sendRecovery(linkRaw, recipient):
+#     link = f"https://stolovaya.online/api/v1/user/recovery?key={linkRaw}"
+#     msg = Message('Ваша личная ссылка в Столовую', recipients=[recipient])
+#     msg.html = f"Здравствуйте<br>Ваша персональная ссылка для входа в аккаунт Столовой <p><small><a href={link}>{link}</small></p>"
+#     current_app.logger.info('msg ready')
+#     mail = current_app.extensions.get('mail')
+#     mail.send(msg)
+#     current_app.logger.info('msg sent')
+
+def policy_get():
+    c = Customer.query.get(int(session['customer']))
+
+    return jsonify({
+        'policy': c.confirmPolicy
+    })
+
+def policy_confirm():
+    c = Customer.query.get(int(session['customer']))
+
+    c.confirmPolicy = True
+    db_commit(c)
+    return jsonify({
+        'code': 'success'
+    })
 
 def return_userid():
     return jsonify({'uid': int(session['customer'])})
@@ -62,8 +90,8 @@ def updateInfo():
     
     customer = Customer.query.get(int(session['customer']))
 
-    if customer.email != email:
-        email_sendRecovery(email, customer.recoveryLink)
+    # if customer.email != email:
+    #     email_sendRecovery(customer.recoveryLink, email)
 
     customer.email = email
     customer.name = name
