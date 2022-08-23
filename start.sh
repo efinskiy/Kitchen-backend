1fc5a72328f0d5a0568e64c5aa0bff589fd81696
Evgeniy <aesto161@gmail.com>
2022-08-23 22:27:35 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export FLASK_DEBUG=1
 export FLASK_APP="application"
-export FLASK_SQLALCHEMY_DATABASE_URI="postgresql://admin:!Finskiy1337@localhost:5432/kitchen_dev"
+export FLASK_SQLALCHEMY_DATABASE_URI="postgresql://login:password@localhost:5432/kitchen_dev"
 export FLASK_SQLALCHEMY_TRACK_MODIFICATIONS=0
 # export FLASK_MAIL_SERVER = ""
 # export FLASK_MAIL_PORT = ""
